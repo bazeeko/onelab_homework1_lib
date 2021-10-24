2fd5bf2d358af2ab7ae8525f061febf056d8cad5
bazeeko <bazarbayev.rus@gmail.com>
2021-10-24 22:35:04 +0600

    onelab_homework1_lib: release of version v1.0.1

--- lib.go
+++ lib.go
@@ -1,5 +1,13 @@
 package onelab_homework1_lib
 
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+var ErrNegativeDiscriminant = errors.New("the quadratic equation has no real roots")
+
 func ChangeLetterCase(str string) string {
 	arrRune := []rune(str)
 
@@ -12,4 +20,21 @@ func ChangeLetterCase(str string) string {
 	}
 
 	return string(arrRune)
+}
+
+func RootsOfQuadraticEquation(a, b, c float64) (x1 float64, x2 float64, err error) {
+	dc := b*b - 4*a*c
+
+	switch {
+	case dc > 0:
+		x1 = (-b + math.Sqrt(dc)) / (2 * a)
+		x2 = (-b - math.Sqrt(dc)) / (2 * a)
+		return
+	case dc == 0:
+		x1 = -b / (2 * a)
+		x2 = -b / (2 * a)
+		return
+	default:
+		return x1, x2, fmt.Errorf("RootsOfQuadraticEquation: %w", ErrNegativeDiscriminant)
+	}
 }
--- lib_test.go
+++ lib_test.go
@@ -1,6 +1,7 @@
 package onelab_homework1_lib
 
 import (
+	"errors"
 	"testing"
 )
 
@@ -13,12 +14,32 @@ func TestChangeLetterCase(t *testing.T) {
 		{"laLAlaA", "LAlaLAa"},
 		{"aAaaaAAaaA", "AaAAAaaAAa"},
 	}
-	// testCase := []string{"alO", "laLAlaA", "aAaaaAAaaA"}
-	// testResult := []string{"ALo", "LAlaLAa", "AaAAAaaAAa"}
 
 	for _, tc := range testCases {
 		if str := ChangeLetterCase(tc.input); str != tc.output {
 			t.Errorf("ChangeLetterCase(%s) => got: %s, want: %s", tc.input, str, tc.output)
+		}
+	}
+}
+
+func TestRootsOfQuadraticEquation(t *testing.T) {
+	testCases := []struct {
+		a   float64
+		b   float64
+		c   float64
+		x1  float64
+		x2  float64
+		err error
+	}{
+		{1, 4, 5, 0, 0, ErrNegativeDiscriminant},
+		{2, 3, -5, 1, -2.5, nil},
+		{1, 2, 1, -1, -1, nil},
+	}
+
+	for _, tc := range testCases {
+		x1, x2, err := RootsOfQuadraticEquation(tc.a, tc.b, tc.c)
+		if x1 != tc.x1 || x2 != tc.x2 || !errors.Is(err, tc.err) {
+			t.Errorf("RootsOfQuadraticEquation(%f, %f, %f) => got (%f, %f, %s), want (%f, %f, %s)", tc.a, tc.b, tc.c, x1, x2, err, tc.x1, tc.x2, tc.err)
 		}
 	}
 }
